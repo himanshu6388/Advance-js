3663df2299b026ca383f3268ea982d4189080248
himanshu6388 <himanshu219181@gmail.com>
2025-04-30 10:07:20 +0530

    updated

--- asyncjs/script.js
+++ asyncjs/script.js
@@ -8,3 +8,6 @@ setTimeout(function(){
 console.log("Hey 3");
 
 // aapke code mein sabse pahle sychronous part chalta hai, synchronoous code ko js chalati hai main thread pe kyuki js single threaded hai , uska likha hua code chalna chaalu hota hai phale and main thred pe hamesha sabse pahle poora code finish hoga tab jaakar async code ke chalne ki baari aayegi
+
+
+// sync code  main thread  mei chalna chaalu ho jaatta hai, and async code ko webAPI behind the scene  operate karna challu kar  deti hai,  jab ye code complete hota  hai tab ye code ka completion  move ho jata hai callback queue main.
