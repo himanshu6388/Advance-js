60430503616961963d6420f3f42262501bcb452d
himanshu6388 <himanshu219181@gmail.com>
2025-05-04 10:31:46 +0530

    Fetching github data

--- asyncjs/script5.js
+++ asyncjs/script5.js
@@ -29,12 +29,34 @@
 //fetch
 // github api
 
-document.querySelector('.btn').addEventListener("click",function(){
+
+
+let result = document.getElementById('result')
+document.getElementById('myform').addEventListener("submit",function(event){
+    event.preventDefault();
+
     
-fetch("https://api.github.com/users/himanshu6388")
+
+    const input = document.getElementById('inputText').value;
+document.querySelector('.btn').addEventListener("click",function(){
+
+fetch(`https://api.github.com/users/${input}`)
 .then(res => res.json()) 
 .then(data =>{
-    console.log(data);
-    
+    result.innerHTML= `
+    <h2>${data.name || data.login}</h2>
+     <img src="${data.avatar_url}" alt="Avatar" width="100">
+        <p><strong>Username:</strong> ${data.login}</p>
+        <p><strong>Bio:</strong> ${data.bio || 'No bio available'}</p>
+        <p><strong>Followers:</strong> ${data.followers}</p>
+        <p><strong>Following:</strong> ${data.following}</p>
+        <p><strong>Public Repos:</strong> ${data.public_repos}</p>
+        <a href="${data.html_url}" target="_blank">View Profile</a>
+    `
 })
 })
+
+
+
+})
+
