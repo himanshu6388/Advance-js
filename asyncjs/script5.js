b8068b4654a69ef220cdd153844b4dc25ea9d142
himanshu6388 <himanshu219181@gmail.com>
2025-05-04 09:39:51 +0530

    Fetching github data

--- asyncjs/script5.js
+++ asyncjs/script5.js
@@ -1,25 +1,40 @@
-function getUserInfo(){
-    return new Promise((res,  rej)=>{
-        setTimeout(() => {
-            res({id:1, name:"himanshu"})
-        }, 1000);
-    })
-}
+// function getUserInfo(){
+//     return new Promise((res,  rej)=>{
+//         setTimeout(() => {
+//             res({id:1, name:"himanshu"})
+//         }, 1000);
+//     })
+// }
 
 
-function getUserPic(id){
-    return new Promise((res, rej)=>{
-        setTimeout(() => {
-            res(["pic1","pic2","pic3"])
-        }, 2000);
-    })
-}
+// function getUserPic(id){
+//     return new Promise((res, rej)=>{
+//         setTimeout(() => {
+//             res(["pic1","pic2","pic3"])
+//         }, 2000);
+//     })
+// }
 
-getUserInfo().then(function(data){
-    console.log(data);
-    return getUserPic(data.id)
-})
-.then(function(data){
+// getUserInfo().then(function(data){
+//     console.log(data);
+//     return getUserPic(data.id)
+// })
+// .then(function(data){
+//     console.log(data);
+    
+// })
+
+
+
+//fetch
+// github api
+
+document.querySelector('.btn').addEventListener("click",function(){
+    
+fetch("https://api.github.com/users/himanshu6388")
+.then(res => res.json()) 
+.then(data =>{
     console.log(data);
     
+})
 })
