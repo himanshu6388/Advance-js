4b85ab4264fd6315eed3ff4485281a1a9c9d7356
himanshu6388 <himanshu219181@gmail.com>
2025-05-04 09:27:43 +0530

    first commit

--- asyncjs/script5.js
+++ asyncjs/script5.js
@@ -1,20 +1,19 @@
-
 function getUserInfo(){
-    return new Promise((res, rej)=>{
+    return new Promise((res,  rej)=>{
         setTimeout(() => {
-            res({id:1,name:"himanshu"});
+            res({id:1, name:"himanshu"})
         }, 1000);
     })
 }
 
+
 function getUserPic(id){
-    return new Promise((res,rej)=>{
+    return new Promise((res, rej)=>{
         setTimeout(() => {
-            res(["pic1","pic2"])
+            res(["pic1","pic2","pic3"])
         }, 2000);
     })
 }
-
 
 getUserInfo().then(function(data){
     console.log(data);
